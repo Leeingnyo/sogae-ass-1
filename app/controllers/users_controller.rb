6b0b38bcf7afa0dc9b52a2613ee8e836cf6569d2
Leeingnyo <dyd1928@naver.com>
2015-03-19 16:43:20 +0900

    add signup and clearData
    signup
    	sign up
    	requires
    	 * username
    	 * password
    	responds
    	 * -1	wrong username length
    	 * -2	worng password length
    	 * -3	username is already used
    
    clearData
    	clear all user data

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -1,2 +1,38 @@
+require 'securerandom'
+require 'digest'
+
 class UsersController < ApplicationController
+	def create
+		username = params[:username]
+		password = params[:password]
+		if not (5 <= username.length and username.length <= 20)
+			#에러코드 -1
+			render json: { error_code: -1 }
+		elsif not (8 <= password.length and password.length <= 20)
+			#에러코드 -2
+			render json: { error_code: -2 }
+		else
+			@user = User.new
+			@user.username = username
+			@user.salt = SecureRandom.hex
+			@user.password = Digest::SHA2.hexdigest(password + @user.salt)
+			@user.count = 1
+
+			if @user.save
+				#성공
+				#유저네임과 카운트 반환
+				session[:user] = @user.id
+				render json: { user_name: @user.username, login_conut: @user.count }
+			else
+				#에러코드 -3
+				render json: { error_code: -3 }
+			end
+		end
+	end
+
+	def clear
+		User.all.each do |x|
+			x.destroy
+		end
+	end
 end
--- config/routes.rb
+++ config/routes.rb
@@ -1,4 +1,6 @@
 Rails.application.routes.draw do
+	post '/signup' => 'users#create'
+	post '/clearData' => 'users#clear'
   # The priority is based upon order of creation: first created -> highest priority.
   # See how all your routes lay out with "rake routes".
 
